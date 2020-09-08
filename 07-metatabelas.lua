809ab3b205d44f0f8163d0d6bb6ba23806dad96d
Celso Crivelaro <celso.crivelaro@revelo.com.br>
2020-09-08 17:57:36 -0300

    Metatables

--- 07-metatabelas.lua
+++ 07-metatabelas.lua
@@ -3,9 +3,21 @@
 x = {value = 5}
 z = {value = 10}
 
+-- https://www.tutorialspoint.com/lua/lua_metatables.htm
+-- https://webserver2.tecgraf.puc-rio.br/lua/local/manual/5.4/manual.html#2.4
 mt = {
   __add = function (a, b) -- "add" event handler
     return { value = a.value + b.value }
+  end,
+  __len = function(obj) 
+    return 8
+  end,
+  __index = function(tabela, chave)
+    if chave == "oi" then
+       return "funciona de jeito diferente"
+    else
+       return tabela[chave]
+    end
   end
 }
 
@@ -14,3 +26,5 @@ setmetatable(z, mt) -- mt é a metatabela de x
 y = x + z
 
 print(y.value)
+print(#z)
+print(x["oi"])
